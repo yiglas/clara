8bf9fdaa5ac140df0c74611402514f57d09e0cf6
yiglas <dsac@hotmail.com>
2016-07-27 15:21:53 -0700

    Updated Install.sh file

--- install.sh
+++ install.sh
@@ -1,1 +1,4 @@
 #  This script should do all the preparation for your project to run, such as downloading any dependencies and compiling if necessary
+
+sudo curl https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein -o /usr/local/bin/lein
+sudo chmod a+x /usr/local/bin/lein
