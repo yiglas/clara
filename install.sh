3d99ba5de983279a00bf791d4aeb195536d69c05
yiglas <dsac@hotmail.com>
2016-07-28 20:58:25 -0700

    Update install

--- install.sh
+++ install.sh
@@ -8,10 +8,10 @@ sudo apt-get update
 
 # download the web service code from git : yet if you are running this script, you've already 
 # - downloaded the code from git.
-# git clone https://github.com/yiglas/clara-challenge-backend.git
+# git clone https://github.com/yiglas/clara-challenge-backend.git giphy-search
 
 # install the latest version of java.
-sudo apt-get install openjdk-9-jre-headless
+sudo apt-get install openjdk-8-jre-headless
 
 # install leiningen.
 sudo curl https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein -o /usr/local/bin/lein
@@ -24,26 +24,26 @@ sudo chmod a+x /usr/local/bin/lein
 # make sure to download all the dependencies for the app
 lein deps
 
-# # compile the web services into a single file.
-# lein uberjar
+# compile the web services into a single file.
+lein uberjar
 
-# # clojure needs two packages
-# sudo apt-get install nginx supervisor
+# clojure needs two packages
+sudo apt-get install nginx supervisor
 
-# # create a place to expose the clojure restful service
-# sudo mkdir -p /var/www/giphy-search/app /var/www/logs
+# create a place to expose the clojure restful service
+sudo mkdir -p /var/www/giphy-search/app /var/www/logs
 
-# # move the clojure application into the newly created folders
-# sudo cp ~/giphy-search/target/giphy-search-0.1.0-SNAPSHOT-standalone.jar /var/www/giphy-search/app/
+# move the clojure application into the newly created folders
+sudo cp ~/giphy-search/target/giphy-search-0.1.0-SNAPSHOT-standalone.jar /var/www/giphy-search/app/
 
-# # the application use www-data, make sure to give it permissions
-# sudo chown -R www-data /var/www/giphy-search/
+# the application use www-data, make sure to give it permissions
+sudo chown -R www-data /var/www/giphy-search/
 
-# cd /var/www/giphy-search
+cd /var/www/giphy-search
 
-# # create a symlink without the version
-# sudo ln -s giphy-search-0.1.0-SNAPSHOT-standalone.jar giphy-search.jar
+# create a symlink without the version
+sudo ln -s giphy-search-0.1.0-SNAPSHOT-standalone.jar giphy-search.jar
 
-# # move the conf file into supervisor
-# sudo cp ~/giphy-search/giphy-serach.conf /etc/supervisor/conf.d/giphy-search.conf
+# move the conf file into supervisor
+sudo cp ~/giphy-search/giphy-serach.conf /etc/supervisor/conf.d/giphy-search.conf
 
